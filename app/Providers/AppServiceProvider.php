40e0c92a353c69ad4041f1a207991db7ed242542
Roberto Barrosss <robertobarros27esp@gmail.com>
2022-02-28 18:45:44 -0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-      //  Schema::defaultStringLength(191); // Acrescentei para corrigir erro com as migrations
+       // Schema::defaultStringLength(191); // Acrescentei para corrigir erro com as migrations
     }
 }
