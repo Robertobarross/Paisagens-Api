3053ce1d20edb59828425c5d83de46172edbaac8
Roberto Barrosss <robertobarros27esp@gmail.com>
2022-02-28 22:19:56 -0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-// use Illuminate\Support\Facades\Schema; // Acrescentei para corrigir erro com as migrations
+use Illuminate\Support\Facades\Schema; // Acrescentei para corrigir erro com as migrations
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-       // Schema::defaultStringLength(191); // Acrescentei para corrigir erro com as migrations
+        Schema::defaultStringLength(191); // Acrescentei para corrigir erro com as migrations
     }
 }
