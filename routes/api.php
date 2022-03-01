91bfe4bb2782177cc662472a2a406448d12dcc4b
Roberto Barrosss <robertobarros27esp@gmail.com>
2022-02-28 22:19:05 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaisagemController; // Acrescentei
 
 Route::get('/paisagens', [PaisagemController::class, 'index']); // Rota principal onde vai aparecer os dados da Api
+
+Route::post('/paisagens', [PaisagemController::class, 'store']); // Insere os dados da API no BD
+
+Route::get('/paisagens/{id}', [PaisagemController::class, 'show']); // Retorna um registro pelo $id
+
+Route::put('paisagens/{id}', [PaisagemController::class, 'update']); // Para editar um registro pelo $id
